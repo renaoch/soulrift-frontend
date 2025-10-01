dd98939e174d5e467f59e7ff18dc8add847d2f2a
Renao <chetriprem.work@gmail.com>
2025-10-01 17:06:41 +0530

    fix supense

--- src/app/collections/layout.tsx
+++ src/app/collections/layout.tsx
@@ -2,12 +2,14 @@
 import type { ReactNode } from "react";
 import Link from "next/link";
 import {NavbarD} from "@/components/Navbar"
+import { Suspense } from 'react';
 export default function ProductsLayout({ children }: { children: ReactNode }) {
   return (
 <div>
     <NavbarD/>
     <main className="flex-1 container mx-auto py-6 px-6">
-        {children}
+          <Suspense fallback={<div>Loading...</div>}> {children}</Suspense>
+       
       </main>
 </div>
 
